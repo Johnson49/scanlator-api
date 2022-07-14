5418b7a8994a67d24c244e2aa112061f1ec1b169
Johnson49 <108435026+Johnson49@users.noreply.github.com>
2022-07-13 23:38:14 -0300

    Removendo trecos de codigo desnecessário

--- src/routes/rota_usuario.ts
+++ src/routes/rota_usuario.ts
@@ -4,26 +4,9 @@ import vereficaCredenciais from "../middleware/verefica_credenciais";
 
 const rota_usuario = Router()
 
-rota_usuario.get('/login', async (req: Request, res: Response) => {
-  
-    try {
-        res.sendFile("../public/index.html")
-    } catch (error) {
-        res.send(error)
-    }
-})
-rota_usuario.get('/imagem', async (req: Request, res: Response) => {
-  
-    try {
-        res.sendFile("../public/img/login.svg")
-    } catch (error) {
-        res.send(error)
-    }
-})
-
 rota_usuario.post('/', vereficaCredenciais,async (req: Request, res: Response) => {
     try {
-        res.send("<h1>bem-vindo</h1>")
+        res.send("")
     } catch (error) {
         res.send(error)
     }
