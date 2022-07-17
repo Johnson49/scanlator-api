eefd150af440ad0bf68ad0f5113dd02861d2fb34
Johnson <johnson@gmail.com>
2022-07-17 18:50:01 -0300

    adicionado as chaves de acesso tanto no config quanto no gitignore

--- src/database/config.js
+++ src/database/config.js
@@ -1,7 +1,7 @@
 import * as admin from 'firebase-admin';
 
 
-const GOOGLE_APPLICATION_CREDENTIALS = "process.env..."
+const GOOGLE_APPLICATION_CREDENTIALS = require("./chave.json")
 
 admin.initializeApp({
   credential: admin.credential.cert(GOOGLE_APPLICATION_CREDENTIALS)
